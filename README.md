a767d441933b030ca7f11fc9047af64292e92976
swaingotnochill <swainroshan001@gmail.com>
2024-02-29 19:47:33 +0530

    feat: initial prototype works

---
+++ README.md
@@ -0,0 +1,13 @@
+## Prototype Review
+
+This tool acts as a preliminary layer to get suggestions for both the owner and the reviewer of the PR.
+
+### Improvements and Feedback.
+
+- Rajeev: 1. Summary of changes made.
+          2. Check coding standards(this vary  from org to org, some standard coding format(gpt can take care of this)).
+
+
+### Will u use it? If yes, why? If no, why?
+
+- Rajeev: Yes. But only when if its freely available or if org provides. It will speed up the review process.
--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 const axios = require("axios");
-
+const { OpenAI } = require("openai");
+const dotenv = require("dotenv");
+dotenv.config();
 // Repository I am using.
 const repo = "https://github.com/tursodatabase/libsql/pull/";
 const PR = 1104;
 
 const project = "tursodatabase/libsql/";
-// Construct the final url.
-const prUrl = repo + PR + ".patch";
 
 // tursodatabase/libsql/pull/1104.patch
 const actualURL = "https://patch-diff.githubusercontent.com/raw/" + project +
   "pull/" + PR + ".patch";
 
-axios.get(actualURL).then((response) => {
-  console.log(response.data);
-}).catch((err) => console.log(err)).finally();
+async function getPRData(url) {
+  try {
+    const response = await axios.get(url);
+    // console.log("Reponse is :\n", response);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
+const systemMessage =
+  `You are an experienced software developer. You will be provided a git .patch file which contains the diff of the code changes made. Please provide whatever code review,
+constructive comments, and optimization that can be possible. Since you don't have access to the whole codebase, refrain from making suggestions if you do not understand the use completly. In such cases
+where you are unsure, just reply: "I do not have access to the entire codebase so cannot review, comment or optimize this code bit."`;
 
+async function makeOpenAIQuery(query, systemMessage) {
+  const OPENAI_KEY = process.env.OPENAI_API_KEY;
+  const openai = new OpenAI({ apiKey: OPENAI_KEY });
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    n: 1,
+    messages: [
+      {
+        role: "system",
+        content: `${systemMessage}`,
+      },
+      {
+        role: "user",
+        content: query,
+      },
+    ],
+  });
+  return completion.choices[0].message?.content;
+}
+
+console.log("Output from OpenAI: \n");
+
+getPRData(actualURL).then(async function (response) {
+  const query = response.data;
+  const data = await makeOpenAIQuery(query, systemMessage);
+  console.log(data);
+}).catch(function (err) {
+  console.log("ERROR: ", err);
+});
